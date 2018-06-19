b3ed264de11c23897e8d36a5b34158911739e617
Jason Turley <jturley@commonapp.local>
2018-06-19 09:56:15 -0400

    Add UserData.cs

---
+++ TCCApplication/Data/UserData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TCCApplication
+{
+    public class UserData
+    {
+
+        /// <summary>
+        /// Required login information
+        /// </summary>
+        public static string Email = "jturley@commonapp.org";
+
+        public static string Password = "C0mm0n@1-2";
+
+        public UserData()
+        {
+            Email = string.Empty;
+            Password = string.Empty;
+        }
+
+        public UserData(string userEmail, string userPassword)
+        {
+            Email = userEmail;
+            Password = userPassword;
+        }
+    }
+}
