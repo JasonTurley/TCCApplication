dcd360f68217b2ab239177c298ef405e2684ac71
Jason Turley <jturley@commonapp.local>
2018-06-19 09:49:37 -0400

    remove unused methods. Added implicit wait wrapper method

--- TCCApplication/DriverUtilities.cs
+++ TCCApplication/DriverUtilities.cs
@@ -4,54 +4,15 @@ using OpenQA.Selenium.Firefox;
 
 namespace TCCApplication
 {
-    public class DriverUtilities
+    public static class DriverUtilities
     {
 
-        private IWebDriver _driver;
+        //private IWebDriver _driver;
 
-        public enum ElementType
+        public static void Wait(IWebDriver driver, double value)
         {
-            ID,
-            NAME,
-            LINKTEXT,
-            XPATH
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(value);
         }
-
-        public DriverUtilities(IWebDriver driver)
-        {
-            this._driver = driver;
-        }
-
-        /// <summary>
-        /// Selects HTML element using IWebdriver instance
-        /// </summary>
-        /// <param name="type"></param>
-        /// <param name="elementName"></param>
-        public void FindElementBy(ElementType type, string elementName)
-        {
-            switch (type)
-            {
-                case ElementType.ID:
-                    _driver.FindElement(By.Id(elementName));            
-                    break;
-                case ElementType.NAME:
-                    _driver.FindElement(By.Name(elementName));
-                    break;
-                case ElementType.LINKTEXT:
-                    _driver.FindElement(By.LinkText(elementName));
-                    break;
-                case ElementType.XPATH:
-                    _driver.FindElement(By.XPath(elementName));
-                    break;
-                default:
-                    throw new Exception("Element type not currently supported.");
-
-            }
-        }
-
-        public void EnterText(string text)
-        {
-
-        }
+        
     }
 }
