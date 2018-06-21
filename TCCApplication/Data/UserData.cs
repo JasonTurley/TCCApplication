200aec4ba12feefafacf8d71622dd9adc1daa4db
Jason Turley <jturley@commonapp.local>
2018-06-21 11:48:11 -0400

    Add properties to user ctor

--- TCCApplication/Data/UserData.cs
+++ TCCApplication/Data/UserData.cs
@@ -8,6 +8,8 @@ namespace TCCApplication.Data
         // For the time being, use mine.
         public UserData()
         {
+            FirstName = "Jason";
+            LastName = "Turley";
             Email = "jturley@commonapp.org";
             Password = "C0mm0n@1-2";
         }
@@ -15,10 +17,11 @@ namespace TCCApplication.Data
         // Email and password required. The other fields can be updated later with UpdateInfo
         public UserData(string userEmail, string userPassword, string firstName = "", string lastName = "", string id = "")
         {
+            this.FirstName = firstName;
+            this.LastName = lastName;
             this.Email = userEmail;
             this.Password = userPassword;
-        }
-
-        
+            this.ID = id;
+        }        
     }
 }
